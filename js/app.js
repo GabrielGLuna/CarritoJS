e97db79b63a4ea126ce93e8d88852857eaa68d35
GabrielGLuna <gabrielunagarcia@hotmail.com>
2023-10-03 12:41:03 -0600

    02 Finaliza carrito

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const listaCursos = document.querySelector('#lista-cursos');
+const tabla = document.querySelector('#lista-carrito tbody');
 
 let carrito = [];
 
@@ -14,23 +15,66 @@ function getCurso(e){
         item.image = padre.parentElement.querySelector('img').src;
         item.cantity = 1;
         // Verificar si el objeto existe
-        if(carrito.some(itemCarrito => item.id === itemCarrito.id)){
-            carrito = carrito.map(itemCarrito => {
-                if(itemCarrito.id === item.id){
-                    itemCarrito.cantity++;
-                    
-                    return itemCarrito;
-                } else {
-                    return itemCarrito;
-                }
-            })
-        }else{
-            carrito.push(item);
-        }
-            
+        addItem(item);
+        showTable();
+    }
+}
+function addItem(item){
+    if(carrito.some(itemCarrito => item.id === itemCarrito.id)){
+        /* carrito = carrito.map(itemCarrito => {
+             if(itemCarrito.id === item.id){
+                 itemCarrito.cantity++;
+                 
+                 return itemCarrito;
+             } else {
+                 return itemCarrito;
+             }
+         })*/
+         carrito.forEach(itemCarrito => {
+             if(itemCarrito.id === item.id){
+                 itemCarrito.cantity ++;
+             }
+         });
+     }else{
+         carrito.push(item);
+     }
+     console.log(carrito);
+}
+function showTable(){
+// Limpiar tabla
+tabla.innerHTML = '';
+//Iterar carrito insertar
+carrito.forEach(item => {
+    tabla.appendChild(createRow(item));
+});
+}
+function createRow(item){
+    const row = document.createElement('tr');
+    let rowHTML = '';
+    rowHTML += `<td><img src =" ${ item.image }" width = "100px"></td>`;
+    rowHTML += `<td> ${ item.name } </td>`;
+    rowHTML += `<td> ${ item.price } </td>`;
+    rowHTML += `<td> ${ item.cantity } </td>`;
+    const buttton = document.createElement('button');
+    buttton.setAttribute('data-id', item.id);
+    buttton.classList.add('btn');
+    buttton.innerHTML = 'X';
+    const td = document.createElement('td');
+    td.appendChild(buttton);
+    row.innerHTML = rowHTML;
+    row.appendChild(td);
+    return row;
 
-        console.log(carrito); 
+}
+
+function btnDEItem(e){
+    if(e.target.classList.contains('btn')){
+        const id = e.target.getAttribute('data-id');
+        //eliminar del carrito}
+        carrito = carrito.filter(itemCarrito => itemCarrito.id !== id);
+        showTable();
     }
 }
 
 listaCursos.addEventListener('click', getCurso);
+tabla.addEventListener('click', btnDEItem);
